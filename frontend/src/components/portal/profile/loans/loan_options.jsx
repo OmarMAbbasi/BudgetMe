d5b52612daa8908308c6470017181d8bfd95cfb9
Omar Abbasi <omarmabbasi@gmail.com>
2019-10-10 03:12:23 -0700

    Set navbar position absolute z axis

---
+++ frontend/src/components/portal/profile/loans/loan_options.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class LoanOptions extends Component {
+	render() {
+		return (
+			<ul>
+				<li>Friends </li>
+				<li>Institutions</li>
+			</ul>
+		);
+	}
+}
